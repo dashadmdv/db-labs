4435b579b1f60f9ae90a88f5bc0f2c0f104b9d30
dashadmdv <apushkins7@gmail.com>
2023-12-20 00:32:38 +0300

    add: CRUD all entities

--- src/controller/cli_controller.py
+++ src/controller/cli_controller.py
@@ -15,3 +15,165 @@ class CLIController:
 
     def close_database(self):
         self.db.close()
+
+    def get_table(self, name):
+        return self.db.show_table(name)
+
+    def get_item_by_id(self, item_id, name):
+        return [self.db.select_by_id(item_id, name)]
+
+    def create_role(self, name):
+        return self.db.create_role(name)
+
+    def create_user(self, username, password, role_id):
+        return self.db.create_user(username, password, role_id)
+
+    def create_user_activity(self, user_id, activity_type, date_of_activity='', time_of_activity=''):
+        return self.db.create_user_activity(user_id, activity_type, date_of_activity, time_of_activity)
+
+    def create_patient(self, user_id, first_name, last_name, date_of_birth, gender, phone_number, email=''):
+        return self.db.create_patient(user_id, first_name, last_name, date_of_birth, gender, phone_number, email)
+
+    def create_doc_category(self, name):
+        return self.db.create_doc_category(name)
+
+    def create_doc_spec(self, name, category_id):
+        return self.db.create_doc_spec(name, category_id)
+
+    def create_department(self, name):
+        return self.db.create_department(name)
+
+    def create_doctor(self, user_id, first_name, last_name, gender, specialization_id, department_id):
+        return self.db.create_doctor(user_id, first_name, last_name, gender, specialization_id, department_id)
+
+    def create_slot(self, date_of_slot, time_of_slot, doctor_id):
+        return self.db.create_slot(date_of_slot, time_of_slot, doctor_id)
+
+    def create_service(self, service_name, price, doctor_id):
+        return self.db.create_service(service_name, price, doctor_id)
+
+    def create_appointment(self, slot_id, patient_id):
+        return self.db.create_appointment(slot_id, patient_id)
+
+    def create_appointment_service(self, appointment_id, service_id):
+        return self.db.create_appointment_service(appointment_id, service_id)
+
+    def create_diagnosis(self, diagnosis_name, diagnosis_code):
+        return self.db.create_diagnosis(diagnosis_name, diagnosis_code)
+
+    def create_prescription(self, note, appointment_id):
+        return self.db.create_prescription(note, appointment_id)
+
+    def create_patient_diagnosis(self, patient_id, diagnosis_id):
+        return self.db.create_patient_diagnosis(patient_id, diagnosis_id)
+
+    # UPDATE
+
+    def update_role(self, id, name):
+        entity = self.get_item_by_id(id, 'role')[0]
+        if entity[1] != name:
+            return self.db.update_role(id, name)
+        return "Nothing to update!"
+
+    def update_user(self, id, username, password, role_id):
+        entity = self.get_item_by_id(id, 'user_acc')[0]
+        username = username or entity[1]
+        password = password or entity[2]
+        role_id = role_id or entity[3]
+        return self.db.update_user(id, username, password, role_id)
+
+    def update_user_activity(self, id, user_id, activity_type, date_of_activity='', time_of_activity=''):
+        entity = self.get_item_by_id(id, 'user_activity')[0]
+        user_id = user_id or entity[1]
+        activity_type = activity_type or entity[2]
+        date_of_activity = date_of_activity or entity[3]
+        time_of_activity = time_of_activity or entity[4]
+        return self.db.update_user_activity(id, user_id, activity_type, date_of_activity, time_of_activity)
+
+    def update_patient(self, id, user_id, first_name, last_name, date_of_birth, gender, phone_number, email=''):
+        entity = self.get_item_by_id(id, 'patient')[0]
+        user_id = user_id or entity[1]
+        first_name = first_name or entity[2]
+        last_name = last_name or entity[3]
+        date_of_birth = date_of_birth or entity[4]
+        gender = gender or entity[5]
+        phone_number = phone_number or entity[6]
+        email = email or entity[7]
+        return self.db.update_patient(id, user_id, first_name, last_name, date_of_birth, gender, phone_number, email)
+
+    def update_doc_category(self, id, name):
+        entity = self.get_item_by_id(id, 'doctor_category')[0]
+        if entity[1] != name:
+            return self.db.update_doc_category(id, name)
+        return "Nothing to update!"
+
+    def update_doc_spec(self, id, name, category_id):
+        entity = self.get_item_by_id(id, 'doctor_specialization')[0]
+        name = name or entity[1]
+        category_id = category_id or entity[2]
+        return self.db.update_doc_spec(id, name, category_id)
+
+    def update_department(self, id, name):
+        entity = self.get_item_by_id(id, 'department')[0]
+        if entity[1] != name:
+            return self.db.update_department(id, name)
+        return "Nothing to update!"
+
+    def update_doctor(self, id, user_id, first_name, last_name, gender, specialization_id, department_id):
+        entity = self.get_item_by_id(id, 'doctor')[0]
+        user_id = user_id or entity[1]
+        first_name = first_name or entity[2]
+        last_name = last_name or entity[3]
+        gender = gender or entity[4]
+        specialization_id = specialization_id or entity[5]
+        department_id = department_id or entity[6]
+        return self.db.update_doctor(id, user_id, first_name, last_name, gender, specialization_id, department_id)
+
+    def update_slot(self, id, date_of_slot, time_of_slot, doctor_id):
+        entity = self.get_item_by_id(id, 'schedule_slot')[0]
+        date_of_slot = date_of_slot or entity[1]
+        time_of_slot = time_of_slot or entity[2]
+        doctor_id = doctor_id or entity[3]
+        return self.db.update_slot(id, date_of_slot, time_of_slot, doctor_id)
+
+    def update_service(self, id, service_name, price, doctor_id):
+        entity = self.get_item_by_id(id, 'service')[0]
+        service_name = service_name or entity[1]
+        price = price or entity[2]
+        doctor_id = doctor_id or entity[3]
+        return self.db.update_service(id, service_name, price, doctor_id)
+
+    def update_appointment(self, id, slot_id, patient_id):
+        entity = self.get_item_by_id(id, 'appointment')[0]
+        slot_id = slot_id or entity[1]
+        patient_id = patient_id or entity[2]
+        return self.db.update_appointment(id, slot_id, patient_id)
+
+    def update_appointment_service(self, id, appointment_id, service_id):
+        entity = self.get_item_by_id(id, 'appointment_service')[0]
+        appointment_id = appointment_id or entity[1]
+        service_id = service_id or entity[2]
+        return self.db.update_appointment_service(id, appointment_id, service_id)
+
+    def update_diagnosis(self, id, diagnosis_name, diagnosis_code):
+        entity = self.get_item_by_id(id, 'diagnosis')[0]
+        diagnosis_name = diagnosis_name or entity[1]
+        diagnosis_code = diagnosis_code or entity[2]
+        return self.db.update_diagnosis(id, diagnosis_name, diagnosis_code)
+
+    def update_prescription(self, id, note, appointment_id):
+        entity = self.get_item_by_id(id, 'prescription')[0]
+        note = note or entity[1]
+        appointment_id = appointment_id or entity[2]
+        return self.db.update_prescription(id, note, appointment_id)
+
+    def update_patient_diagnosis(self, id, patient_id, diagnosis_id):
+        entity = self.get_item_by_id(id, 'patient_diagnosis')[0]
+        patient_id = patient_id or entity[1]
+        diagnosis_id = diagnosis_id or entity[2]
+        return self.db.update_patient_diagnosis(id, patient_id, diagnosis_id)
+
+    # DELETE
+
+    def delete_by_id(self, item_id, table):
+        return self.db.delete_by_id(item_id, table)
--- src/storage/database.py
+++ src/storage/database.py
@@ -50,3 +50,402 @@ class Database:
     def query(self, sql, params=None):
         self.cursor.execute(sql, params or ())
         return self.fetchall()
+
+    # READ
+
+    def show_table(self, name):
+        return self.query(f"SELECT * FROM {name} ORDER BY id;")
+
+    def select_by_id(self, item_id, table):
+        sql = f"SELECT * FROM {table} WHERE id = {item_id};"
+        self.execute(sql)
+        return self.fetchone()
+
+    # CREATE
+
+    def create_role(self, name):
+        try:
+            sql = f"""
+                        INSERT INTO role (role_name)
+                        VALUES ('{name}');
+                    """
+            self.execute(sql)
+            return "Role added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_user(self, username, password, role_id):
+        try:
+            sql = f"""
+                        INSERT INTO user_acc (username, password, role_id)
+                        VALUES ('{username}', '{password}', {role_id});
+                    """
+            self.execute(sql)
+            return "Role added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_user_activity(self, user_id, activity_type, date_of_activity='', time_of_activity=''):
+        try:
+            sql = f"""
+                        INSERT INTO user_activity (user_id, activity_type, date_of_activity, time_of_activity)
+                        VALUES ({user_id}, '{activity_type}', '{date_of_activity}', '{time_of_activity}');
+                    """
+            self.execute(sql)
+            return "User activity added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_patient(self, user_id, first_name, last_name, date_of_birth, gender, phone_number, email=''):
+        try:
+            sql = f"""
+                        INSERT INTO patient (user_id, first_name, last_name, date_of_birth, gender, phone_number, email)
+                        VALUES ({user_id}, '{first_name}', '{last_name}', '{date_of_birth}', '{gender}', '{phone_number}', '{email}');
+                    """
+            self.execute(sql)
+            return "Patient added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_doc_category(self, name):
+        try:
+            sql = f"""
+                        INSERT INTO doctor_category (category_name)
+                        VALUES ('{name}');
+                    """
+            self.execute(sql)
+            return "Doctor category added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_doc_spec(self, name, category_id):
+        try:
+            sql = f"""
+                        INSERT INTO doctor_specialization (specialization_name, category_id)
+                        VALUES ('{name}', {category_id});
+                    """
+            self.execute(sql)
+            return "Doctor specialization added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_department(self, name):
+        try:
+            sql = f"""
+                        INSERT INTO department (department_name)
+                        VALUES ('{name}');
+                    """
+            self.execute(sql)
+            return "Department added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_doctor(self, user_id, first_name, last_name, gender, specialization_id, department_id):
+        try:
+            sql = f"""
+                        INSERT INTO doctor (user_id, first_name, last_name, gender, specialization_id, department_id)
+                        VALUES ({user_id}, '{first_name}', '{last_name}', '{gender}', {specialization_id}, {department_id});
+                    """
+            self.execute(sql)
+            return "Doctor added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_slot(self, date_of_slot, time_of_slot, doctor_id):
+        try:
+            sql = f"""
+                        INSERT INTO schedule_slot (date_of_slot, time_of_slot, doctor_id)
+                        VALUES ('{date_of_slot}', '{time_of_slot}', {doctor_id});
+                    """
+            self.execute(sql)
+            return "Schedule slot added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_service(self, service_name, price, doctor_id):
+        try:
+            sql = f"""
+                        INSERT INTO service (service_name, price, doctor_id)
+                        VALUES ('{service_name}', {price}, {doctor_id});
+                    """
+            self.execute(sql)
+            return "Service added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_appointment(self, slot_id, patient_id):
+        try:
+            sql = f"""
+                        INSERT INTO appointment (slot_id, patient_id)
+                        VALUES ({slot_id}, {patient_id});
+                    """
+            self.execute(sql)
+            return "Appointment added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_appointment_service(self, appointment_id, service_id):
+        try:
+            sql = f"""
+                        INSERT INTO appointment_service (appointment_id, service_id)
+                        VALUES ({appointment_id}, {service_id});
+                    """
+            self.execute(sql)
+            return "Service added to appointment!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_diagnosis(self, diagnosis_name, diagnosis_code):
+        try:
+            sql = f"""
+                        INSERT INTO appointment_service (appointment_id, service_id)
+                        VALUES ('{diagnosis_name}', '{diagnosis_code}');
+                    """
+            self.execute(sql)
+            return "Diagnosis added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_prescription(self, note, appointment_id):
+        try:
+            sql = f"""
+                        INSERT INTO prescription (note, appointment_id)
+                        VALUES ('{note}', {appointment_id});
+                    """
+            self.execute(sql)
+            return "Prescription added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def create_patient_diagnosis(self, patient_id, diagnosis_id):
+        try:
+            sql = f"""
+                        INSERT INTO patient_diagnosis (patient_id, diagnosis_id)
+                        VALUES ({patient_id}, {diagnosis_id});
+                    """
+            self.execute(sql)
+            return "Diagnosis added to patient!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    # UPDATE
+
+    def update_role(self, id, name):
+        try:
+            sql = f"""
+                        UPDATE role
+                        SET role_name='{name}'
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Role updated!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_user(self, id, username, password, role_id):
+        try:
+            sql = f"""
+                        UPDATE user_acc 
+                        SET username='{username}', password='{password}', role_id={role_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Role added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_user_activity(self, id, user_id, activity_type, date_of_activity='', time_of_activity=''):
+        try:
+            sql = f"""
+                        UPDATE user_activity 
+                        SET user_id={user_id}, activity_type='{activity_type}', date_of_activity='{date_of_activity}', 
+                        time_of_activity='{time_of_activity}'
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "User activity added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_patient(self, id, user_id, first_name, last_name, date_of_birth, gender, phone_number, email=''):
+        try:
+            sql = f"""
+                        UPDATE patient 
+                        SET user_id={user_id}, first_name='{first_name}', last_name='{last_name}', 
+                        date_of_birth='{date_of_birth}', gender='{gender}', phone_number='{phone_number}', email='{email}')
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Patient added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_doc_category(self, id, name):
+        try:
+            sql = f"""
+                        UPDATE doctor_category 
+                        SET category_name='{name}'
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Doctor category added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_doc_spec(self, id, name, category_id):
+        try:
+            sql = f"""
+                        UPDATE doctor_specialization 
+                        SET specialization_name='{name}', category_id={category_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Doctor specialization added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_department(self, id, name):
+        try:
+            sql = f"""
+                        UPDATE department 
+                        SET department_name='{name}'
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Department added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_doctor(self, id, user_id, first_name, last_name, gender, specialization_id, department_id):
+        try:
+            sql = f"""
+                        UPDATE doctor 
+                        SET user_id={user_id}, first_name='{first_name}', last_name='{last_name}', gender='{gender}', 
+                        specialization_id={specialization_id}, department_id={department_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Doctor added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_slot(self, id, date_of_slot, time_of_slot, doctor_id):
+        try:
+            sql = f"""
+                        UPDATE schedule_slot 
+                        SET date_of_slot='{date_of_slot}', time_of_slot='{time_of_slot}', doctor_id={doctor_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Schedule slot added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_service(self, id, service_name, price, doctor_id):
+        try:
+            sql = f"""
+                        UPDATE service 
+                        SET service_name='{service_name}', price={price}, doctor_id={doctor_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Service added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_appointment(self, id, slot_id, patient_id):
+        try:
+            sql = f"""
+                        UPDATE appointment 
+                        SET slot_id={slot_id}, patient_id={patient_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Appointment added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_appointment_service(self, id, appointment_id, service_id):
+        try:
+            sql = f"""
+                        UPDATE appointment_service 
+                        SET appointment_id={appointment_id}, service_id={service_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Service added to appointment!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_diagnosis(self, id, diagnosis_name, diagnosis_code):
+        try:
+            sql = f"""
+                        UPDATE appointment_service 
+                        SET appointment_id='{diagnosis_name}', service_id='{diagnosis_code}'
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Diagnosis added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_prescription(self, id, note, appointment_id):
+        try:
+            sql = f"""
+                        UPDATE prescription 
+                        SET note='{note}', appointment_id={appointment_id}
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Prescription added!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    def update_patient_diagnosis(self, id, patient_id, diagnosis_id):
+        try:
+            sql = f"""
+                        UPDATE patient_diagnosis 
+                        SET patient_id={patient_id}, diagnosis_id={diagnosis_id})
+                        WHERE id={id};
+                    """
+            self.execute(sql)
+            return "Diagnosis added to patient!"
+        except Exception as e:
+            self.execute("ROLLBACK")
+            return e
+
+    # DELETE
+
+    def delete_by_id(self, item_id, table):
+        sql = f"DELETE FROM {table} WHERE id = {item_id};"
+        self.execute(sql)
+        return "Item deleted!"
